f39728e98549fae118bdad5b6b38da8b5f041e56
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-14 14:47:01 +0800

    Update seqlist.h

--- Data Structure/1. Linear List/1.1 Sequence List/seqlist.h
+++ Data Structure/1. Linear List/1.1 Sequence List/seqlist.h
@@ -1,4 +1,14 @@
 template <class Type> class SeqList
 {
+private:
+	Type *elem;
+	int length;
+	int listsize;
+	
 public:
 	SeqList(int size);
+	~SeqList(void) {delete [] elem;}
+	
+	int seqListEmpty(void) const {return length == 0;}
+	
+};
