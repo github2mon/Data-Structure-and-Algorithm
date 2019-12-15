28982c4902c0857062774ce82485f1d705ab36a0
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-15 19:18:44 +0800

    Update linklist.h

--- Data Structure/1. Linear List/1.2 Linked List/linklist.h
+++ Data Structure/1. Linear List/1.2 Linked List/linklist.h
@@ -1,10 +1,23 @@
-template <class Type> class LinkList;
-template <class Type> class ListNode
-{
+#include<iostream>
+using namespace std;
 
+template <typename T> class LinkList;
+template <typename T> class ListNode
+{
+	friend class LinkList<T>;
+private:
+	T data;
+	ListNode<T> *next;
+public:
+	ListNode() : next(NULL) {};
+	ListNOde(T &e) : data(e), next(NULL) {};
+	T& getNodeData() {return data;}
+	ListNode<T>* getNodePtr() {return next;}
+	void setNodeData(T& e) {data = e;}
+	void setNodePtr(ListNode<T>* ptr) {next = ptr;}
 };
 
-template <class Type> class LinkList
+template <typename T> class LinkList
 {
 
 };
