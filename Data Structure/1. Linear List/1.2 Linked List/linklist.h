bce5cc52d2b1a0cdf0ae7774e384bd5fba530b33
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-15 23:52:13 +0800

    Update linklist.h

--- Data Structure/1. Linear List/1.2 Linked List/linklist.h
+++ Data Structure/1. Linear List/1.2 Linked List/linklist.h
@@ -19,5 +19,22 @@ public:
 
 template <typename T> class LinkList
 {
-
+private:
+	ListNode<T>* head;
+public:
+	LinkList() {head = new ListNode<T>();}
+	LinkList<T>(LinkList<T>& List);
+	~LinkList() {clearList(); delete head;}
+	void clearList();
+	int getLength() const;
+	ListNode<T>* locateElem(T e) const;
+	ListNode<T>* getElem(int i) const;
+	int insert(int i, T& e);
+	T* delete(int i);
+	ListNode<T>* remove(int i);
+	int replace(int i, T& e);
+	ListNode<T>* prior(int i);
+	ListNode<T>* next(int i);
+	bool isEmpty(void);
+	LinkList<T>& operate = (LinkNode<T>& List);
 };
