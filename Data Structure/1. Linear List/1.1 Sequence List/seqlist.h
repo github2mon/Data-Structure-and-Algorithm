a5ffb1230548748a482b61355132ae51080f9eaa
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-16 22:25:54 +0800

    Update seqlist.h

--- Data Structure/1. Linear List/1.1 Sequence List/seqlist.h
+++ Data Structure/1. Linear List/1.1 Sequence List/seqlist.h
@@ -24,3 +24,66 @@ public:
 	//application
 	void merge(SeqList<Type> &la, SeqList<Type> &lb, SeqList<Type> &lc);
 };
+
+template<class Type> SeqList<Type>::SeqList(int size) {
+	elem = new Type[size];
+	if (!elem) { cout << "overflow"; exit(0); }
+	listsize = size;
+	length = 0;
+}
+
+template<class Type> int SeqList<Type>::locateElem(Type e) {
+	if (length == 0) return 0;
+	int i = 0;
+	while (i < length && e != elem[i]) i++;
+	if (i < length) return i;
+	else return 0;
+}
+
+template<class Type> int SeqList<Type>::replaceElem(int i, Type &e) {
+	if (i < length) { elem[i] = e; return 1;}
+	return 0;
+}
+
+template<class Type> int SeqList<Type>::insertElem(int i, Type &e) {
+	if (i < 0 || i > length) return 0;
+	if (listsize == length) //add more storage space
+	{
+		Type *p = new Type[listsize++];
+		if (!p) { cout << "overflow"; exit(0); }
+		for (int j = 0; j < length; j++)
+			p[j] = elem[j];
+		delete[] elem;
+		elem = p;
+		p = NULL;
+	}
+	for (int j = length - 1; j >= i; j--) //backwards elems
+		elem[j + 1] = elem[j];
+	elem[i] = e;
+	length++;
+	return 1;
+}
+
+template<class Type> int SeqList<Type>::deleteElem(int i, Type &e) {
+	if (i < 0 || i > length - 1) return 0;
+	e = elem[i];
+	for (int j = i; j < length - 1; j++) //forwards elems
+		elem[j] = elem[j + 1];
+	length--;
+	return 1;
+}
+
+//application
+template<class Type> void SeqList<Type>::merge(SeqList<Type> &la, SeqList<Type> &lb, SeqList<Type> &lc) {
+	int la_length = la.getLength();
+	int lb_length = lb.getLength();
+	for (int i = 0; i < la_length; i++)
+		lc.insertElem(i, la.getElem(i));
+	int lc_length = lc.getLength();
+	for (int i = 0; i < lb_length; i++)
+	{
+		int data = lb.getElem(i);
+		if (!lc.locateElem(data))
+			lc.insertElem(lc_length++, data);
+	}
+}
