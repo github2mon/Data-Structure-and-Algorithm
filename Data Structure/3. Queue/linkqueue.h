16719f7ef870ffd6b24d1469256bb3c7c095a05c
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-24 23:44:11 +0800

    Update linkqueue.h

--- Data Structure/3. Queue/linkqueue.h
+++ Data Structure/3. Queue/linkqueue.h
@@ -10,7 +10,7 @@ private:
 	QueueNode<T>* next;
 
 public:
-	QueueNode(T& e, QueueNode<T>* p == NULL)
+	QueueNode(T& e, QueueNode<T>* p = NULL)
 	{
 		data = e;
 		next = p;
@@ -30,5 +30,53 @@ public:
 	T& frontElem();
 	void enQueue(T& e);
 	T& deQueue();
-	void clearQueue();
 };
+
+template <typename T> LinkQueue<T>::~LinkQueue()
+{
+	QueueNode<T>* p;
+	while (front)
+	{
+		p = front;
+		front = front->next;
+		delete p;
+	}
+}
+
+template <typename T> int LinkQueue<T>::length() const
+{
+	QueueNode<T>* p = front;
+	int cnt = 0;
+	while (p)
+	{
+		cnt++;
+		p = p->next;
+	}
+	return cnt;
+}
+
+template <typename T> T& LinkQueue<T>::frontElem()
+{
+	if (!front) { cout << "Queue is empty"; exit(0); }
+	return front->data;
+}
+
+template <typename T> void LinkQueue<T>::enQueue(T& e)
+{
+	if (!front)
+		front = tail = new QueueNode<T>(e, NULL);
+	else
+		tail = tail->next = new QueueNode<T>(e, NULL);
+}
+
+template <typename T> T& LinkQueue<T>::deQueue()
+{
+	if(!front) { cout << "Queue is empty"; exit(0); }
+	QueueNode<T>* p = front;
+	front = front->next;
+	if (tail == p) tail = front;
+	T* q = new T;
+	q = p->data;
+	delete p;
+	return *q;
+}
